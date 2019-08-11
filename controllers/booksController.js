495f3a689b9978b53b98c59c14964768a968fe68
Mayra Cuenca <mayra.cuencac@gmail.com>
2019-08-11 15:42:01 -0500

    Minor changes to booksController, updates to package-lock.json

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -14,6 +14,7 @@ module.exports = {
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
+      console.log("Error por aquí. Find by Id");
   },
   create: function(req, res) {
     db.Book
